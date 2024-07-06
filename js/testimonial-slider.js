8e03578be905b9750e1e63c7e0b37e228188209f
Md-Neamul-Hoqe <hoqe1997@gmail.com>
2024-07-06 19:30:57 +0600

    [UI](footer): footer section implemented

---
+++ js/testimonial-slider.js
@@ -0,0 +1,17 @@
+function rotateOrder(direction) {
+    const container = document.getElementById('testimonial-container');
+    const slides = container.getElementsByClassName('testimonial-slide');
+    const totalSlides = slides.length;
+
+    for (let slide of slides) {
+        let currentOrder = parseInt(window.getComputedStyle(slide).order);
+        if (direction === 'next') {
+            slide.style.order = (currentOrder % totalSlides) + 1;
+        } else if (direction === 'prev') {
+            slide.style.order = (currentOrder - 2 + totalSlides) % totalSlides + 1;
+        }
+        console.log(slide.style);
+    }
+
+}
+
